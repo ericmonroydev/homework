ca1c8f3493f803562168643120ac0d884e97c994
ericmonroydev <ericmonroydev@gmail.com>
2023-04-12 21:03:17 -0600

    inicio de javascript

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// uso de la consola
+
+// Abrimos con  console y añadim os el método
+// metodo abreviado en teclado  clg
+console.log();
+
+// Uso de let Y const
+function imprimiNombre() {
+    const nombre = document.getElementById("nombre");
+    nombre.innerHTML = "Eric Monroy";
+    edad(35);
+}
+
+function edad(a) {
+    const edad = document.getElementById("edad");
+    edad.innerHTML = a;
+}
+
+
+imprimiNombre();
+
+
+
+
